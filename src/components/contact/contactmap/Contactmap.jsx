d64e30f40758e1f5b2844751d0200d6290c8206c
G3orgeS <112631563+G3orgeS@users.noreply.github.com>
2023-05-19 21:01:44 +0200

    iframe spökar, måste felsöka, kommenterade bort det

--- src/components/contact/contactmap/Contactmap.jsx
+++ src/components/contact/contactmap/Contactmap.jsx
@@ -5,13 +5,13 @@ const Contactmap = () => {
   return (
 
 <div className='iframe-wrapper'>
-     <iframe 
+     {/* <iframe 
      width="800" 
      height="500" 
-     frameborder="0" 
+     frameBorder="0" 
      src="https://www.bing.com/maps/embed?h=500&w=800&cp=59.308828~18.026912&lvl=15&typ=d&sty=r&src=SHELL&FORM=MBEDV8" 
      scrolling="yes">
-     </iframe>
+     </iframe> */}
 </div>   
 
   )
