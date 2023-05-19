8f9d5de28ef3046001a4a4a66738d34aebb8e66a
G3orgeS <112631563+G3orgeS@users.noreply.github.com>
2023-05-19 20:41:01 +0200

    karta tillagd

--- src/components/contact/contactmap/Contactmap.jsx
+++ src/components/contact/contactmap/Contactmap.jsx
@@ -4,9 +4,15 @@ import '../contactmap/contactmap.css'
 const Contactmap = () => {
   return (
 
-    <div className="iframe-wrapper">
-      <iframe className="iframe" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d509.1146933469845!2d18.026767417652266!3d59.30859591584008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d721efbe7ef%3A0x6b3dbf3bd8af6f17!2sKYH%20yrkesh%C3%B6gskola!5e0!3m2!1ssv!2sse!4v1673910914787!5m2!1ssv!2sse"></iframe>
-    </div>    
+<div className='iframe-wrapper'>
+     <iframe 
+     width="800" 
+     height="500" 
+     frameborder="0" 
+     src="https://www.bing.com/maps/embed?h=500&w=800&cp=59.308828~18.026912&lvl=15&typ=d&sty=r&src=SHELL&FORM=MBEDV8" 
+     scrolling="yes">
+     </iframe>
+</div>   
 
   )
 }
