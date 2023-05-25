5a86c2e7de3356218e60f67af78dd302b9a4f7ef
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-25 15:04:38 +0200

    commit to merge with master

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,24 +6,32 @@ import RelatedProducts from '../components/products/relatedProducts/RelatedProdu
 import SmallProductCarousell from '../components/products/smallCarousell/SmallProductCarousell'
 import useDoc from '../hooks/useDoc'
 import Loader from '../components/loader/Loader'
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation } from 'react-router-dom'
 
 
 
 function ProductDetails({ products }) {
   
-  // console.log(id)
   
-  const { id } = useParams()
-  let [ _id, set_id] = useState(id);
-
-  const { data: product, error, loading } = useDoc('products', _id)
+  const { id } = useParams();
+  
+  const { data: product, error, loading } = useDoc('products', id)
+  
   if (!product) return (
     <div>
       {loading && <Loader />}
       {error && <p>{error}</p>}
     </div>
+
   )
+    
+  
+  
+
+  
+  
+
+
 
 
   let matches = []
@@ -39,7 +47,7 @@ function ProductDetails({ products }) {
     }
 
     productSerachFunction()
-    console.log(matches)
+    // console.log(matches)
 
   return (
     <>
@@ -53,63 +61,3 @@ function ProductDetails({ products }) {
 }
 
 export default ProductDetails
-
-
-// import { useParams } from 'react-router-dom'
-// import useDoc from '../hooks/useDoc'
-// import React, { useState } from 'react'
-// import WideHero from '../components/universal/wideHero/WideHero'
-// import Details from '../components/products/productDetails/Details'
-// import ProductInfo from '../components/products/productInformation/ProductInfo'
-// // import RelatedProducts from '../components/products/relatedProducts/RelatedProducts'
-// import SmallProductCarousell from '../components/products/smallCarousell/SmallProductCarousell'
-// import Loader from '../components/loader/Loader'
-// import RelatedProducts from '../components/products/relatedProducts/RelatedProducts'
-
-
-// function ProductDetails({ products }) {
-
-
-  
-//   const { id } = useParams()
-//   const { data: product, error, loading } = useDoc('product', id)
-  
-  
-  
-//   if (!product) return (
-//     <div>
-//       {loading && <Loader />}
-//       {error && <p>{error}</p>}
-//     </div>
-//   )
-
-
-//   // console.log(product)
-//   // console.log(product.category)
-//   // const [category, setCategory] = useState(product.category)
-
-//   let matches = []
-    
-//   // Searching the products array categories to find search word
-//   // const productSerachFunction = () => {
-//   //     products.forEach(product => {
-//   //       if(product.category.includes('Footwear')){
-//   //         return matches.push(product)
-//   //       }
-//   //     });
-//   //   }
-
-//   //   productSerachFunction()
-
-//   return (
-//     <>
-//       <WideHero key={'wideHeroProductID'} product={product} />
-//       <Details key={'detailsProductID'} product={product} />
-//       <ProductInfo key={'productInfoProductID'} product={product} />
-//       {/* <RelatedProducts key={matches._id} matches={matches} /> */}
-//       <SmallProductCarousell key={'smallProductCarousellProductID'} product={product} />
-//     </>
-//   )
-// }
-
-// export default ProductDetails
